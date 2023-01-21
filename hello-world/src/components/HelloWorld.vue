ac1ff4a4afcd30cb556bc770992cdbbff65c2c71
TomyJusuf <82041257+TomyJusuf@users.noreply.github.com>
2023-01-21 20:45:24 +0100

    make navbar with media query responsive layout
    create animate hamburget for close our navbar in media query

--- hello-world/src/components/HelloWorld.vue
+++ hello-world/src/components/HelloWorld.vue
@@ -1,58 +1,134 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <nav>
+    <div class="navbar">
+      <div class="log">Logo</div>
+      <div class="cross">
+        <div class="a"></div>
+      </div>
+      <div class="info">
+        <li>Home</li>
+        <li>Info</li>
+        <li>Galery</li>
+        <li>Shop</li>
+        <li>Contact</li>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+nav {
+  margin: 0px 10px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.navbar {
+  display: flex;
+  padding: 10px;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 25px;
+  flex-wrap: wrap;
+}
+.log {
+  display: inline-block;
+}
+.info {
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 li {
-  display: inline-block;
-  margin: 0 10px;
+  width: 90px;
+  height: 100%;
+  margin: 10px 10px;
+  list-style-type: none;
 }
-a {
-  color: #42b983;
+
+@media (max-width: 650px) {
+  .cross {
+    position: absolute;
+    width: 50px;
+    height: 50px;
+    background-color: #fff;
+    top: 1%;
+    left: 88%;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+  }
+  .a {
+    width: 40px;
+    height: 1px;
+    padding: 1px;
+    background-color: #000;
+    color: #000;
+  }
+  .a::before {
+    content: "";
+    width: 10px;
+    height: 1px;
+    padding: 1px;
+    background-color: #000;
+    color: #000;
+    position: absolute;
+    margin-top: 10px;
+  }
+  .a::after {
+    position: absolute;
+    margin-top: -10px;
+    content: "";
+    width: 40px;
+    height: 1px;
+    padding: 1px;
+    background-color: #000;
+    color: #000;
+  }
+  nav {
+    height: 100%;
+    /* background: red; */
+    flex-wrap: wrap;
+    display: flex;
+  }
+  navbar {
+    align-items: center;
+    row-gap: 5px;
+  }
+  .log {
+    margin: 0 auto 10px;
+    width: 450px;
+    height: 50px;
+    padding-top: 5px;
+  }
+  .info {
+    width: 350px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    margin: 0 auto;
+    row-gap: 5px;
+    align-content: center;
+  }
+  li {
+    /* display: none; */
+    display: block;
+    align-items: center;
+    padding-top: 5px;
+    margin: 1px auto;
+    min-width: 350px;
+    height: 50px;
+    /* text-decoration: underline; */
+    border-bottom-width: 1px;
+    border-bottom-style: solid;
+    text-transform: uppercase;
+  }
 }
 </style>
