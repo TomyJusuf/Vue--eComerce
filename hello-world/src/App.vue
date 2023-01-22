15c5589c4a12496e2b27a17ba18068cd1e68627a
TomyJusuf <82041257+TomyJusuf@users.noreply.github.com>
2023-01-22 16:46:46 +0100

    use v-bind:class for underline Text

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,15 +1,13 @@
 <template>
-  <Navbar />
-
-  <!-- <div>{{ greet }} {{ name }}</div>
-  <div v-html="channel"></div>
-  <div v-text="channel"></div>
+  <!-- <Navbar /> -->
   <h2 v-bind:id="headingId">Heading</h2>
-  <button v-bind:disabled="isDisables">Bind</button> -->
+  <button v-bind:disabled="isDisabled">Bind</button>
+  <h2 class="underline">Underline text</h2>
+  <h2 v-bind:class="underline">Text</h2>
 </template>
 
 <script>
-import Navbar from "./components/Navbar.vue";
+// import Navbar from "./components/Navbar.vue";
 export default {
   name: "App",
   data() {
@@ -18,10 +16,11 @@ export default {
       name: "Tomas",
       channel: "<b>Coco-Logo</b>",
       headingId: "heading",
-      isDisables: false,
+      isDisabled: true,
+      underline: "underline",
     };
   },
-  components: { Navbar },
+  // components: { Navbar },
 };
 </script>
 
@@ -40,5 +39,8 @@ export default {
   color: #fff;
   margin-top: 0px;
   background-color: #61a4e7;
+}
+.underline {
+  text-decoration: underline;
 }
 </style>
