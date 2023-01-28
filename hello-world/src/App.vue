3f1bbbcd826e9df0db5be1fcde14b6f297a2bf65
TomyJusuf <82041257+TomyJusuf@users.noreply.github.com>
2023-01-28 14:12:18 +0100

    learn about watch property

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -2,24 +2,12 @@
   <!-- <Navbar /> -->
   <!-- <CardSection /> -->
   <!-- <Formular /> -->
-
-  <h2>fullname- {{ firstName }} {{ lastName }}</h2>
-  <h2>Computed fullname- {{ fullName }}</h2>
-  <button @click="changeFullName">Change fullName</button>
-
-  <button @click="items.push({ id: 4, title: 'Keyboard', price: 50 })">
-    Add item
-  </button>
-  <h2>Computed total - {{ total }}</h2>
-  <h2>Method Total - {{ getTotal() }}</h2>
-  <input type="text" v-model="country" />
-
-  <template v-for="item in items" :key="item.id">
-    <h2 v-if="item.price > 100">{{ item.title }}{{ item.price }}</h2>
-  </template>
-  <h2 v-for="item in expensiveItems" :key="item.id">
-    {{ item.title }}{{ item.price }}
-  </h2>
+  <h1>Volume Tracker (0-20)</h1>
+  <h3>Current Volume - {{ volume }}</h3>
+  <div>
+    <button @click="volume += 2">Increment</button>
+    <button @click="volume -= 2">Decrement</button>
+  </div>
 </template>
 
 <script>
@@ -30,64 +18,15 @@ export default {
   name: "App",
   data() {
     return {
-      firstName: "Bruse",
-      lastName: "Wayne",
-      items: [
-        {
-          id: 1,
-          title: "TV",
-          price: 100,
-        },
-        {
-          id: 2,
-          title: "Phone",
-          price: 200,
-        },
-        {
-          id: 3,
-          title: "Laptop",
-          price: 300,
-        },
-      ],
-      country: "",
+      volume: 0,
     };
   },
-  methods: {
-    getTotal() {
-      return this.items.reduce(
-        (total, current) => (total = total + current.price),
-        0
-      );
-    },
-    changeFullName() {
-      this.fullName = "Clark Kent 30";
-    },
-  },
-  computed: {
-    fullName: {
-      get() {
-        return `${this.age} ${this.firstName} ${this.lastName} `;
-      },
-      set(value) {
-        const name = value.split(" ");
-        this.firstName = name[0];
-        this.lastName = name[1];
-        this.age = name[2];
-      },
-    },
-    total() {
-      return this.items.reduce(
-        (total, current) => (total = total + current.price),
-        0
-      );
-    },
-    expensiveItems() {
-      return this.items.filter((item) => item.price > 100);
-    },
-  },
-
-  // components: { Navbar, CardSection },
+  methods: {},
+  computed: {},
+  watch: {},
 };
+
+// components: { Navbar, CardSection },
 </script>
 
 <style>
